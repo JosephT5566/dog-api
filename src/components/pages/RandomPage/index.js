6fb9367d14d6b408d017b457161f8fc041687f44
Your JosephTseng <zxp930110@hotmail.com.tw>
2020-08-13 17:08:38 +0800

    Fix double click issue of get random image 1. create component <RandomImage/> again, use `useRef` to monitor if component has been updated 2. catch async error

--- src/components/pages/RandomPage/index.js
+++ src/components/pages/RandomPage/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const RandomPage = () => {
     const [imageUrl, setImageUrl] = useState('');
@@ -7,47 +7,66 @@ const RandomPage = () => {
         getImage();
     }, []);
 
-    // useEffect(() => {
-    //     console.log(imageUrl);
-    // }, [imageUrl]);
-
     const getImage = async () => {
         setImageUrl('');
-        const { message: image } = await (
-            await fetch('https://dog.ceo/api/breeds/image/random', {})
-        ).json();
-        // console.log(image);
-        setImageUrl(image);
+        try {
+            const { message: image } = await (
+                await fetch('https://dog.ceo/api/breeds/image/random', {})
+            ).json();
+            setImageUrl(image);
+        } catch (err) {
+            alert(err);
+        }
+    };
+
+    const renderPlaceholder = () => {
+        return (
+            <div className="ui placeholder" style={{ margin: '0 auto' }}>
+                <div className="rectangular image"></div>
+            </div>
+        );
     };
 
     const renderImage = () => {
-        if (imageUrl === '') {
-            return (
-                <div className="ui placeholder" style={{ margin: '0 auto' }}>
-                    <div className="rectangular image"></div>
-                </div>
-            );
-        } else {
-            return (
-                <img
-                    className="ui medium centered rounded image"
-                    src={imageUrl}
-                    alt={imageUrl}
-                />
-            );
-        }
+        return imageUrl === '' ? (
+            renderPlaceholder()
+        ) : (
+            <RandomImage imageUrl={imageUrl} />
+        );
     };
 
     return (
         <div>
-            <h1 className="i header">Get Random Image</h1>
-            <button
-                className="ui orange button"
-                onClick={getImage}
-            >
+            <h1 className="ui header">Get Random Image</h1>
+            <button className="ui orange button" onClick={getImage}>
                 Get Image
             </button>
             {renderImage()}
+        </div>
+    );
+};
+
+const RandomImage = ({ imageUrl }) => {
+    const [url, setUrl] = useState(null);
+    const ref = useRef(true);
+
+    useEffect(() => {
+        if (ref.current) {
+            setUrl(imageUrl);
+        }
+
+        return () => {
+            ref.current = false;
+        };
+    }, [imageUrl]);
+
+    return (
+        <div>
+            <img
+                className="ui medium centered rounded image"
+                src={url}
+                alt={url}
+            />
         </div>
     );
 };
